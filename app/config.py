270bff1ca3577d893e83481a8724e480faf8634a
chilln <kimjihan77@naver.com>
2025-02-17 15:44:22 +0900

    feat:환경변수 설정

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # .env 파일에서 환경 변수 로드
+
+def _getenv(name: str, default: str = None) -> str | None:
+    value = os.getenv(name, default)
+    return value
+
+
+MONGO_URI = _getenv("MONGO_URI")
+MONGO_NAME = _getenv("MONGO_NAME")
