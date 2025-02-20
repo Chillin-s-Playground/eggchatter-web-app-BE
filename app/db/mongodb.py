328c878a82f0353fbbab52dff2ad94e82dbd8d81
chilln <kimjihan77@naver.com>
2025-02-20 23:01:39 +0900

    feat:ODM model 정의 및 DB connection, session 관리 함수

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,16 +1,60 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from beanie import init_beanie
+from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.database import Database as MongoDatabase
 
 from app.config import MONGO_NAME, MONGO_PASSWORD, MONGO_URI, MONGO_USER
+from app.models.models import Chats, EasterEggs, Users
 
 
 async def _check_connection():
     """DB 커넥션 테스트"""
     client = AsyncIOMotorClient(
-        f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URI}/{MONGO_NAME}"
+        f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URI}?authSource={MONGO_USER}"
     )
+    client[MONGO_NAME]
 
     try:
-        info = await client.server_info()
+        await client.server_info()
         print("MongoDB 연결 성공")
     except Exception as e:
-        print("MongoDB 연결 실패")
+        print("MongoDB 연결 실패", str(e))
+
+
+class MongoDB:
+    def __init__(self) -> None:
+        self._client = AsyncIOMotorClient(
+            f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URI}?authSource={MONGO_USER}"
+        )
+        self._db = self._client[MONGO_NAME]
+
+    @asynccontextmanager
+    async def start_session(self):
+        """MongoDB 연결 및 session 관리"""
+        session = None
+        try:
+            session = await self._client.start_session()
+            await init_beanie(
+                self._db, document_models=[Users, EasterEggs, Chats]
+            )  # session 없이 DB만 초기화
+            yield session  # 세션을 반환
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            raise e
+        finally:
+            # 트랜잭션이 완료된 후 세션 종료
+            if session:
+                await session.end_session()
+                print("end session")
+
+
+mongo_client = MongoDB()
+
+
+async def get_db() -> AsyncGenerator:
+    async with mongo_client.start_session() as session:
+        db = mongo_client._db
+        yield db
