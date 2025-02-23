8578fd5b76af3486b7f572b1a51590c0ad58ca06
chilln <kimjihan77@naver.com>
2025-02-23 22:16:38 +0900

    feat:NotFoundError 생성

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -12,6 +12,15 @@ class DuplicatedError(HTTPException):
         super().__init__(status.HTTP_400_BAD_REQUEST, detail, headers)
 
 
+class NotFoundError(HTTPException):
+    """찾을 수 없는 데이터 오류"""
+
+    def __init__(
+        self, detail: Optional[str] = None, headers: Optional[Dict[str, Any]] = None
+    ):
+        super().__init__(status.HTTP_404_NOT_FOUND, detail, headers)
+
+
 class UnknownError(HTTPException):
     """알 수 없는 서버오류"""
 
