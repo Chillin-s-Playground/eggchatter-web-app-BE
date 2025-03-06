5467494900b944e2364ef0e94a6f60182b9b4ab0
chilln <kimjihan77@naver.com>
2025-03-06 10:55:53 +0900

    DB 연결 및 세션관리 메소드 수정 - mongoDB에서 mySQL로 DB엔진 변경했음

--- app/core/database.py
+++ app/core/database.py
@@ -1,60 +1,32 @@
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from contextlib import contextmanager
+from typing import Generator
 
-from beanie import init_beanie
-from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.database import Database as MongoDatabase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
-from app.core.config import MONGO_NAME, MONGO_PASSWORD, MONGO_URI, MONGO_USER
-from app.models.chats import Chats
-from app.models.easter_egg import EasterEggs
-from app.models.users import Users
+from app.core.config import configs
+
+engine = create_engine(configs.DATABASE_URI, echo=True)
+
+SessionLocal = sessionmaker(bind=engine, class_=Session, expire_on_commit=False)
 
 
-async def _check_connection():
-    """DB 커넥션 테스트"""
-    client = AsyncIOMotorClient(
-        f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URI}?authSource={MONGO_USER}"
-    )
-    client[MONGO_NAME]
-
+@contextmanager
+def start_session():
+    """MySQL 연결 및 session 관리"""
+    session = None
     try:
-        await client.server_info()
-        print("MongoDB 연결 성공")
+        session = SessionLocal()
+        yield session
     except Exception as e:
-        print("MongoDB 연결 실패", str(e))
+        print(f"에러 : {str(e)}")
+        raise e
+    finally:
+        if session:
+            session.close()
 
 
-class MongoDB:
-    def __init__(self) -> None:
-        self._client = AsyncIOMotorClient(
-            f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URI}?authSource={MONGO_USER}"
-        )
-        self._db = self._client[MONGO_NAME]
-
-    @asynccontextmanager
-    async def start_session(self):
-        """MongoDB 연결 및 session 관리"""
-        session = None
-        try:
-            session = await self._client.start_session()
-            await init_beanie(self._db, document_models=[Users, EasterEggs, Chats])
-            yield session
-        except Exception as e:
-            print(f"Error: {str(e)}")
-            raise e
-        finally:
-            # 트랜잭션이 완료된 후 세션 종료
-            if session:
-                await session.end_session()
-                print("end session")
-
-
-mongo_client = MongoDB()
-
-
-async def get_db() -> AsyncGenerator:
-    async with mongo_client.start_session() as session:
-        db = mongo_client._db
-        yield db
+def get_db() -> Generator:
+    """API 호출시 발동될 메소드"""
+    with start_session() as session:
+        yield session
