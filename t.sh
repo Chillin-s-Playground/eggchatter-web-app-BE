d313204d6478c455a84430039548095ea075475b
chilln <kimjihan77@naver.com>
2025-03-07 17:14:03 +0900

    feat:pytest 스크립트

---
+++ t.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+tcps="test_create_profile_success"
+tcpf="test_create_profile_failure"
+
+_usage() {
+  echo "Usage: $0 -s <test_file> -f <test_function_alias>"
+  echo "Example: $0 -s users -f tcps"
+  exit 1
+}
+
+while getopts "s:f:" opt; do
+  case ${opt} in
+    s)
+      TEST_FILE=$OPTARG
+      ;;
+    f)
+      TEST_FUNCTION_ALIAS=$OPTARG
+      ;;
+    *)
+      _usage
+      ;;
+  esac
+done
+
+if [[ -z "$TEST_FILE" || -z "$TEST_FUNCTION_ALIAS" ]]; then
+  _usage
+fi
+
+TEST_FUNCTION=${!TEST_FUNCTION_ALIAS}
+if [[ -z "$TEST_FUNCTION" ]]; then
+  echo "Invalid function alias: $TEST_FUNCTION_ALIAS"
+  exit 1
+fi
+
+pytest "app/tests/test_${TEST_FILE}.py::$TEST_FUNCTION"
